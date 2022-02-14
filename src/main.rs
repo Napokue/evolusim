e32e56a1c4addc381748758cced7b271580e7608
Timo de Kort <dekort.timo@gmail.com>
2022-02-14 22:08:33 +0100

    impl Bevy book example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Human 1".to_string()));
+    commands.spawn().insert(Person).insert(Name("Human 2".to_string()));
+    commands.spawn().insert(Person).insert(Name("Human 3".to_string()));
+}
+
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>) {
+
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
 }
