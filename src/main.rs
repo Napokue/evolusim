d844739d3329a68a565e9f2ebf3fae9e6d57e6c4
Timo de Kort <dekort.timo@gmail.com>
2022-02-15 21:24:27 +0100

    Add static sprite and keyboard input

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,40 @@
+
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup)
+        .add_system(keyboard_input_system)
         .run();
 }
 
-pub struct HelloPlugin;
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+}
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app
-            .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
+fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.any_pressed([KeyCode::W, KeyCode::Up]) {
+        info!("W or Up");
     }
-}
 
-#[derive(Component)]
-struct Person;
+    if keyboard_input.any_pressed([KeyCode::A, KeyCode::Left]) {
+        info!("A or Left");
+    }
 
-#[derive(Component)]
-struct Name(String);
+    if keyboard_input.any_pressed([KeyCode::S, KeyCode::Down]) {
+        info!("S or Down");
+    }
 
-struct GreetTimer(Timer);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Human 1".to_string()));
-    commands.spawn().insert(Person).insert(Name("Human 2".to_string()));
-    commands.spawn().insert(Person).insert(Name("Human 3".to_string()));
-}
-
-fn greet_people(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    query: Query<&Name, With<Person>>) {
-
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
+    if keyboard_input.any_pressed([KeyCode::D, KeyCode::Right]) {
+        info!("D or Right");
     }
 }
