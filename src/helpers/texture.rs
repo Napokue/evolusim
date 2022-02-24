b857bc8344ef9aeb25d8566f4460b8141f27c14b
Timo de Kort <dekort.timo@gmail.com>
2022-02-24 22:12:28 +0100

    Add tile system

---
+++ src/helpers/texture.rs
@@ -0,0 +1,23 @@
+/*
+Credits to texture help class of bevy_ecs_tilemap: https://github.com/StarArawn/bevy_ecs_tilemap/blob/main/examples/helpers/texture.rs
+ */
+use bevy::{prelude::*, render::render_resource::TextureUsages};
+
+pub fn set_texture_filters_to_nearest(
+    mut texture_events: EventReader<AssetEvent<Image>>,
+    mut textures: ResMut<Assets<Image>>,
+) {
+    // quick and dirty, run this for all textures anytime a texture is created.
+    for event in texture_events.iter() {
+        match event {
+            AssetEvent::Created { handle } => {
+                if let Some(mut texture) = textures.get_mut(handle) {
+                    texture.texture_descriptor.usage = TextureUsages::TEXTURE_BINDING
+                        | TextureUsages::COPY_SRC
+                        | TextureUsages::COPY_DST;
+                }
+            }
+            _ => (),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,124 @@
-use bevy::app::CreatePlugin;
-use bevy::core::FixedTimestep;
 use bevy::prelude::*;
-use std::fmt::Error;
+use bevy_ecs_tilemap::prelude::*;
 
-const TIME_STEP: f32 = 1.0 / 60.0;
-const SCREEN_WIDTH: usize = 128;
-const SCREEN_HEIGHT: usize = 128;
+mod helpers;
+
+const SCREEN_WIDTH: f32 = 720.0;
+const SCREEN_HEIGHT: f32 = 720.0;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            width: SCREEN_WIDTH,
+            height: SCREEN_HEIGHT,
+            title: String::from("Evolusim"),
+            resizable: false,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system_set(
-            SystemSet::new()
-                .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                .with_system(creature_movement_system),
-        )
-        .add_system(keyboard_input_system)
+        .add_plugin(TilemapPlugin)
+        .add_startup_system(startup)
+        .add_system(helpers::camera::movement)
+        .add_system(helpers::texture::set_texture_filters_to_nearest)
+        .add_system(update)
         .run();
 }
 
-#[derive(Component)]
-struct Cell {}
-
-#[derive(Component)]
-struct GridLayout {
-    cells: [[Cell; SCREEN_WIDTH]; SCREEN_HEIGHT],
-}
-
-impl GridLayout {
-    pub fn new(mut commands: Commands) -> Result<Self, Error> {
-        let cells = [[Cell {}; SCREEN_WIDTH]; SCREEN_HEIGHT];
-
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
-                commands.insert(cells[y][x])
-            }
-        }
-
-        Ok(GridLayout {
-            cells: [[Cell {}; SCREEN_WIDTH]; SCREEN_HEIGHT],
-        })
-    }
-}
-
-#[derive(Component)]
-struct Creature {
-    velocity: Vec3,
-}
-
-fn setup(mut commands: Commands) {
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query: MapQuery) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    let texture_handle = asset_server.load("creatures.png");
+
+    let chunk_size = Amount::new(2, 2);
+    let tile_amount = Amount::new(8, 8);
+    let tile_size = 45.;
+
+    let map_size = MapSize(
+        chunk_size.width * tile_amount.width,
+        chunk_size.height * tile_amount.height,
+    );
+
+    // Create map entity and component:
+    let map_entity = commands.spawn().id();
+    let mut map = Map::new(0u16, map_entity);
+
+    let (mut layer_builder, layer_entity) = LayerBuilder::<TileBundle>::new(
+        &mut commands,
+        LayerSettings::new(
+            MapSize(chunk_size.width, chunk_size.height),
+            ChunkSize(tile_amount.width, tile_amount.height),
+            TileSize(tile_size, tile_size),
+            TextureSize(270., tile_size),
+        ),
+        0u16,
+        0u16,
+    );
+
+    let mut i = 0;
+    for x in 0..map_size.0 {
+        for y in 0..map_size.1 {
+            // Ignore errors for demo sake.
+            let _ = layer_builder.set_tile(
+                TilePos(x, y),
+                Tile {
+                    texture_index: 0,
+                    visible: true,
+                    ..Default::default()
+                }
+                .into(),
+            );
+            i += 1;
+        }
+    }
+
+    map_query.build_layer(&mut commands, layer_builder, texture_handle.clone());
+
+    commands.entity(layer_entity).insert(LastUpdate(0.0));
+
+    // Required to keep track of layers for a map internally.
+    map.add_layer(&mut commands, 0u16, layer_entity);
+
+    // Spawn Map
+    // Required in order to use map_query to retrieve layers/tiles.
     commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgb(0.25, 0.25, 0.75),
-                custom_size: Some(Vec2::new(50.0, 50.0)),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(Creature {
-            velocity: 400.0 * Vec3::new(0.5, -0.5, 0.0).normalize(),
-        })
-        .insert(GridLayout::new(commands));
+        .entity(map_entity)
+        .insert(map)
+        .insert(Transform::from_xyz(
+            0. - SCREEN_WIDTH / 2.,
+            0. - SCREEN_HEIGHT / 2.,
+            0.0,
+        ))
+        .insert(GlobalTransform::default());
 }
 
-fn creature_movement_system(mut creature_query: Query<(&Creature, &mut Transform)>) {
-    let (creature, mut transform) = creature_query.single_mut();
-    transform.translation += creature.velocity * TIME_STEP;
+fn update(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut last_update_query: Query<&mut LastUpdate>,
+    tile_query: Query<(Entity, &Tile, &TilePos)>,
+    mut map_query: MapQuery,
+) {
 }
 
-// TODO Implement camera panning
-fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>) {
-    if keyboard_input.any_pressed([KeyCode::W, KeyCode::Up]) {
-        info!("W or Up");
-    }
+#[derive(Component)]
+pub struct LastUpdate(f64);
 
-    if keyboard_input.any_pressed([KeyCode::A, KeyCode::Left]) {
-        info!("A or Left");
-    }
+pub struct Amount {
+    pub width: u32,
+    pub height: u32,
+}
 
-    if keyboard_input.any_pressed([KeyCode::S, KeyCode::Down]) {
-        info!("S or Down");
+impl Amount {
+    fn new(width: u32, height: u32) -> Self {
+        Self { width, height }
     }
+}
 
-    if keyboard_input.any_pressed([KeyCode::D, KeyCode::Right]) {
-        info!("D or Right");
+impl Default for Amount {
+    fn default() -> Self {
+        Self {
+            width: 0,
+            height: 0,
+        }
     }
 }
